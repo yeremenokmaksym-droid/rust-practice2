9cce9e6bd732aa0e570cdc49658c334ca864cf69
yeremenokmaksym-droid <yeremenok.maksym@e-u.edu.ua>
2026-04-13 19:15:33 +0300

    Update task03.rs

--- src/hackerrank/task03.rs
+++ src/hackerrank/task03.rs
@@ -1,1 +1,17 @@
 // https://www.hackerrank.com/challenges/staircase/problem
+using System;
+
+class Solution { static void Main() { int n = int.Parse(Console.ReadLine());
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+            Console.Write(" ");
+
+        for (int j = 0; j < i; j++)
+            Console.Write("#");
+
+        Console.WriteLine();
+    }
+}
+}
