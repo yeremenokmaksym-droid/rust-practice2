03c561d35d8004c33cfe146e44a6e7a6f8d95213
yeremenokmaksym-droid <yeremenok.maksym@e-u.edu.ua>
2026-05-07 14:01:29 +0300

    Update and rename task09.rs to task09.cs

---
+++ src/hackerrank/task09.cs
@@ -0,0 +1,71 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<int> ar)
+    {
+
+{
+    int pairs = 0;
+    List<int> colors = new List<int>();
+
+    foreach (int color in ar)
+    {
+        if (colors.Contains(color))
+        {
+            pairs++;
+            colors.Remove(color);
+        }
+        else
+        {
+            colors.Add(color);
+        }
+    }
+
+    return pairs;
+}
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+// https://www.hackerrank.com/challenges/migratory-birds/problem
