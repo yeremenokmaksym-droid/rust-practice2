b59915579fd151b1beb4ff4a2ff4b2b8cf1bbe2c
yeremenokmaksym-droid <yeremenok.maksym@e-u.edu.ua>
2026-06-02 10:28:57 +0300

    Update task09.cs

--- src/hackerrank/task09.cs
+++ src/hackerrank/task09.cs
@@ -15,14 +15,7 @@ using System;
 class Result
 {
 
-    /*
-     * Complete the 'sockMerchant' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts following parameters:
-     *  1. INTEGER n
-     *  2. INTEGER_ARRAY ar
-     */
+    
 
     public static int sockMerchant(int n, List<int> ar)
     {
